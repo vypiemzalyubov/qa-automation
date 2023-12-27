6a7f9fe142fa67637a77a6f66cd9308691bce670
Dmitriy Nikolaev <dnikolaev@tradingview.com>
2023-12-27 14:57:16 +0300

    Add 1 task

---
+++ Selenium/Selenium course by Alexey Koledachkin/01. Initializing Chrome and Firefox drivers/main.py
@@ -0,0 +1,22 @@
+# Chrome first method
+
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+# Chrome second method
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+# Firefox
+
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+
+service = Service("/snap/bin/geckodriver")
+driver = webdriver.Firefox(service=service)
