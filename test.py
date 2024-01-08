a7db9346d2fa8f17a3a9c2687a5bcf570d989c08
vypiemzalyubov <dima.rza90@mail.ru>
2024-01-08 20:55:26 +0300

    Update module 8

--- test.py
+++ test.py
@@ -21,25 +21,29 @@ BASE_URL = "https://www.amazon.com/"
 
 driver.get(BASE_URL)
 
-print("Start test: Setting and reading cookies")
-driver.add_cookie({
-    "name": "username",
-    "value": "user123"
-})
-driver.refresh()
-assert driver.get_cookie("username")["name"] == "username", \
-    "No cookie named \"username\""
-assert driver.get_cookie("username")["value"] == "user123", \
-    "No cookie with the value of \"user123\""
-print(f'"username" cookie: {driver.get_cookie("username")}')
-print("Finish test: Setting and reading cookies")
+# print("Start test: Setting and reading cookies")
+# driver.add_cookie({
+#     "name": "username",
+#     "value": "user123"
+# })
+# driver.refresh()
+# assert driver.get_cookie("username")["name"] == "username", \
+#     "No cookie named \"username\""
+# assert driver.get_cookie("username")["value"] == "user123", \
+#     "No cookie with the value of \"user123\""
+# print(f'"username" cookie: {driver.get_cookie("username")}')
+# print("Finish test: Setting and reading cookies")
 
-print("Start test: Deleting cookies")
-all_cookies_before = driver.get_cookies()
-all_cookie_names_before = [cookie["name"] for cookie in all_cookies_before]
-driver.delete_cookie(all_cookie_names_before[0])
-all_cookies_after = driver.get_cookies()
-all_cookie_names_after = [cookie["name"] for cookie in all_cookies_after]
-assert all_cookie_names_before[0] not in all_cookie_names_after, \
-    f"The \"{all_cookie_names_before[0]}\" cookie is there, but it shouldn't be"
-print("Finish test: Deleting cookies")
+# print("Start test: Deleting cookies")
+# all_cookies_before = driver.get_cookies()
+# all_cookie_names_before = [cookie["name"] for cookie in all_cookies_before]
+# driver.delete_cookie(all_cookie_names_before[0])
+# all_cookies_after = driver.get_cookies()
+# all_cookie_names_after = [cookie["name"] for cookie in all_cookies_after]
+# assert all_cookie_names_before[0] not in all_cookie_names_after, \
+#     f"The \"{all_cookie_names_before[0]}\" cookie is there, but it shouldn't be"
+# print("Finish test: Deleting cookies")
+
+print("Start test: Shopping cart automation")
+
+print("Finish test: Shopping cart automation")
