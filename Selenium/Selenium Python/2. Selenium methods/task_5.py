f121a80cf06cfb34910eca5cc8628b20c3289a46
vypiemzalyubov <dima.rza90@mail.ru>
2024-03-28 19:34:18 +0300

    Add task_5.py

--- Selenium/Selenium Python/2. Selenium methods/task_5.py
+++ Selenium/Selenium Python/2. Selenium methods/task_5.py
@@ -8,9 +8,22 @@ chrome_options.add_argument('--window-size=1920,1080')
 chrome_options.add_argument('--disable-cache')
 chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 
-INPUT = ('xpath', '//input[@type="text"]')
-SUBMIT_BTN = ('xpath', '//button[@id="checkButton"]')
+LINK = ('xpath', '//a')
+TEXT = ('xpath', '//p[@id="result"]')
 
 with webdriver.Chrome(options=chrome_options) as driver:
     wait = WebDriverWait(driver, 15, poll_frequency=1)
     driver.get('https://parsinger.ru/methods/5/index.html')
+
+    cookie_dict = {}
+
+    links = wait.until(EC.visibility_of_all_elements_located(LINK))
+    for link in links:
+        link.click()
+        expiry = driver.get_cookies()[0]['expiry']
+        text = wait.until(EC.visibility_of_element_located(TEXT)).text
+        cookie_dict[text] = expiry
+        driver.back()
+
+    result = sorted(cookie_dict.items(), key=lambda x: x[1], reverse=True)[0][0]
+    print(f'Result: {result}')
