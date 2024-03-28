ec7f415e29faa61e00772750a060f53a5bc4a02d
Dmitriy Nikolaev <dnikolaev@tradingview.com>
2024-03-28 07:33:47 +0300

    Add task_1.py

--- Selenium/Selenium Python/2. Selenium methods/task_1.py
+++ Selenium/Selenium Python/2. Selenium methods/task_1.py
@@ -8,8 +8,17 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-cache')
 chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 
-
+RESULT = ('xpath', '//p[@id="result"]')
 
 with webdriver.Chrome(options=chrome_options) as driver:
     wait = WebDriverWait(driver, 15, poll_frequency=1)
     driver.get('https://parsinger.ru/methods/1/index.html')
+
+    default_text = wait.until(EC.visibility_of_element_located(RESULT)).text
+
+    while True:
+        result = wait.until(EC.visibility_of_element_located(RESULT)).text
+        if result != default_text:
+            print(f'Result: {result}')
+            break
+        driver.refresh()
