96474f30984b7369096d0bc8921675761fc5e016
Dmitriy Nikolaev <dnikolaev@tradingview.com>
2024-03-29 18:22:08 +0300

    Finish task_10.py

--- Selenium/Selenium Python/2. Selenium methods/task_10.py
+++ Selenium/Selenium Python/2. Selenium methods/task_10.py
@@ -12,3 +12,12 @@ chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 with webdriver.Chrome(options=chrome_options) as driver:
     wait = WebDriverWait(driver, 15, poll_frequency=1)
     driver.get('https://parsinger.ru/methods/3/index.html')
+
+    result = 0
+
+    cookies = driver.get_cookies()
+    for cookie in cookies:
+        if 'secret_cookie' in cookie['name']:
+            result += int(cookie['value'])
+
+    print(f'Result: {result}')
