e7c173d8fa17acc19c3473a6739d1e95031c994b
Dmitriy Nikolaev <dnikolaev@tradingview.com>
2024-03-29 11:52:45 +0300

    Add task_7.py

--- Selenium/Selenium Python/2. Selenium methods/task_7.py
+++ Selenium/Selenium Python/2. Selenium methods/task_7.py
@@ -8,7 +8,19 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-cache')
 chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 
+CHECKBOX = ('xpath', '//input[@type="checkbox"]')
 
 with webdriver.Chrome(options=chrome_options) as driver:
     wait = WebDriverWait(driver, 15, poll_frequency=1)
     driver.get('https://parsinger.ru/selenium/5.5/3/1.html')
+
+    result = 0
+
+    checkboxes = wait.until(EC.visibility_of_all_elements_located(CHECKBOX))
+    for i, checkbox in enumerate(checkboxes, start=1):
+        if checkbox.is_selected():
+            result += int(
+                wait.until(EC.visibility_of_element_located(('xpath', f'//div[@class="parent"][{i}]//textarea'))).text
+            )
+
+    print(f'Result: {result}')
