1e3bf334eb0341e7d628dfe29eaddae21d769f85
vypiemzalyubov <dima.rza90@mail.ru>
2024-03-30 12:14:01 +0300

    Add task_11.py

--- Selenium/Selenium Python/2. Selenium methods/task_10.py
+++ Selenium/Selenium Python/2. Selenium methods/task_10.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 chrome_options = Options()
