6f9994ac221e6539a9d35672194ad252c5f3f4c3
Dmitriy Nikolaev <dnikolaev@tradingview.com>
2024-04-01 16:41:00 +0300

    Update task_3.py

--- Selenium/Selenium Python/3. Page Scrolling/task_3.py
+++ Selenium/Selenium Python/3. Page Scrolling/task_3.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.common import TimeoutException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
@@ -10,9 +9,21 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-cache')
 chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 
-
+DIV = ('xpath', '//div[@id="scroll-container"]/div')
+SCROLL_CONTAINER = ('xpath', '//div[@id="scroll-container"]')
 
 with webdriver.Chrome(options=chrome_options) as driver:
     wait = WebDriverWait(driver, 15, poll_frequency=1)
     actions = ActionChains(driver)
     driver.get('https://parsinger.ru/infiniti_scroll_2/')
+
+    for i in range(100):
+        actions \
+            .move_to_element(wait.until(EC.presence_of_element_located(DIV))) \
+            .scroll_by_amount(0, 1) \
+            .pause(1) \
+            .perform()
+
+    numbers = wait.until(EC.presence_of_element_located(SCROLL_CONTAINER)).text
+    result = sum(int(item) for item in numbers.split())
+    print(f'Result: {result}')
