8fab4732c721032ff97d2b4dbb3ed5b07a017ad0
Dmitriy Nikolaev <dnikolaev@tradingview.com>
2024-04-02 15:43:45 +0300

    Add task_5.py

--- Selenium/Selenium Python/3. Page Scrolling/task_6.py
+++ Selenium/Selenium Python/3. Page Scrolling/task_6.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
@@ -8,18 +10,8 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-cache')
 chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 
-BTN = ('xpath', '//button[@class="clickMe"]')
 
 with webdriver.Chrome(options=chrome_options) as driver:
     wait = WebDriverWait(driver, 15, poll_frequency=1)
-    driver.get('https://parsinger.ru/selenium/5.7/1/index.html')
-
-    button_list = wait.until(EC.presence_of_all_elements_located(BTN))
-    for button in button_list:
-        driver.execute_script("return arguments[0].scrollIntoView(true);", button)
-        button.click()
-
-    alert = wait.until(EC.alert_is_present())
-    alert_text = alert.text
-    alert.accept()
-    print(f'Result: {alert_text}')
+    actions = ActionChains(driver)
+    driver.get('https://parsinger.ru/selenium/5.7/5/index.html')
