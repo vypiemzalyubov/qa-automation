5a18b5d799c51735de8173a6c3d1ff71cfa47222
dnikolaev <dnikolaev@tradingview.com>
2024-04-03 15:41:23 +0300

    Add task_2.py, task_3.py

--- Selenium/Selenium Python/4. Windows and Tabs/task_2.py
+++ Selenium/Selenium Python/4. Windows and Tabs/task_2.py
@@ -9,18 +9,24 @@ chrome_options.add_argument('--disable-cache')
 chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 
 BTN = ('xpath', '//input[@type="button"]')
+INPUT = ('xpath', '//input[@id="input"]')
+CHECK_BTN = ('xpath', '//input[@value="Проверить"]')
 RESULT = ('xpath', '//p[@id="result"]')
 
 with webdriver.Chrome(options=chrome_options) as driver:
     wait = WebDriverWait(driver, 15, poll_frequency=1)
-    driver.get('https://parsinger.ru/selenium/5.8/1/index.html')
+    driver.get('https://parsinger.ru/selenium/5.8/2/index.html')
 
     button_list = wait.until(EC.presence_of_all_elements_located(BTN))
     for button in button_list:
         button.click()
-        wait.until(EC.alert_is_present()).accept()
+        alert = wait.until(EC.alert_is_present())
+        alert_text = alert.text
+        alert.accept()
+        wait.until(EC.visibility_of_element_located(INPUT)).send_keys(alert_text)
+        wait.until(EC.visibility_of_element_located(CHECK_BTN)).click()
 
         result = wait.until(EC.presence_of_element_located(RESULT)).text
-        if result:
+        if result != 'Неверный пин-код':
             print(f'Result: {result}')
             break
--- Selenium/Selenium Python/4. Windows and Tabs/task_3.py
+++ Selenium/Selenium Python/4. Windows and Tabs/task_3.py
@@ -8,19 +8,23 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-cache')
 chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 
-BTN = ('xpath', '//input[@type="button"]')
+PIN = ('xpath', '//span[@class="pin"]')
+CHECK_BTN = ('xpath', '//input[@value="Проверить"]')
 RESULT = ('xpath', '//p[@id="result"]')
 
 with webdriver.Chrome(options=chrome_options) as driver:
     wait = WebDriverWait(driver, 15, poll_frequency=1)
-    driver.get('https://parsinger.ru/selenium/5.8/1/index.html')
+    driver.get('https://parsinger.ru/selenium/5.8/3/index.html')
 
-    button_list = wait.until(EC.presence_of_all_elements_located(BTN))
-    for button in button_list:
-        button.click()
-        wait.until(EC.alert_is_present()).accept()
+    pin_list = wait.until(EC.visibility_of_all_elements_located(PIN))
+    for pin in pin_list:
+        pin_text = pin.text
+        wait.until(EC.visibility_of_element_located(CHECK_BTN)).click()
+        prompt = wait.until(EC.alert_is_present())
+        prompt.send_keys(pin_text)
+        prompt.accept()
 
         result = wait.until(EC.presence_of_element_located(RESULT)).text
-        if result:
+        if result != 'Неверный пин-код':
             print(f'Result: {result}')
             break
