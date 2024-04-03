c74931dcb6494a5f3e534278500553b26a6c733a
dnikolaev <dnikolaev@tradingview.com>
2024-04-03 16:50:53 +0300

    Add task_6.py

--- Selenium/Selenium Python/4. Windows and Tabs/task_6.py
+++ Selenium/Selenium Python/4. Windows and Tabs/task_6.py
@@ -13,3 +13,17 @@ RESULT = ('xpath', '//span[@id="result"]')
 with webdriver.Chrome(options=chrome_options) as driver:
     wait = WebDriverWait(driver, 15, poll_frequency=1)
     driver.get('https://parsinger.ru/window_size/2/index.html')
+
+    window_size_x = [616, 648, 680, 701, 730, 750, 805, 820, 855, 890, 955, 1000]
+    window_size_y = [300, 330, 340, 388, 400, 421, 474, 505, 557, 600, 653, 1000]
+
+    for x in window_size_x:
+        for y in window_size_y:
+            driver.set_window_size(x, y)
+
+            result = wait.until(EC.presence_of_element_located(RESULT)).text
+            if result:
+                print(
+                    f"Result: {{'width': {driver.execute_script('return window.innerWidth;')}, 'height': {driver.execute_script('return window.innerHeight;')}}}"
+                )
+                break
