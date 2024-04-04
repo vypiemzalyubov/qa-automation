0127baa00a59be6cf8def776409b71c05d47841f
dnikolaev <dnikolaev@tradingview.com>
2024-04-04 07:48:08 +0300

    Add task_7.py

--- Selenium/Selenium Python/4. Windows and Tabs/task_7.py
+++ Selenium/Selenium Python/4. Windows and Tabs/task_7.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,8 +10,20 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-cache')
 chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 
-RESULT = ('xpath', '//span[@id="result"]')
+BTN = ('xpath', '//input[@type="button"]')
 
 with webdriver.Chrome(options=chrome_options) as driver:
     wait = WebDriverWait(driver, 15, poll_frequency=1)
     driver.get('https://parsinger.ru/blank/3/index.html')
+
+    result = 0
+
+    button_list = wait.until(EC.visibility_of_all_elements_located(BTN))
+    for i, button in enumerate(button_list, start=1):
+        button.click()
+        driver.window_handles
+        driver.switch_to.window(driver.window_handles[i])
+        result += int(driver.title)
+        driver.switch_to.window(driver.window_handles[0])
+
+    print(f'Result: {result}')
