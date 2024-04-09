e606e035cbe53016a882c3426f86fbba547604e9
dnikolaev <dnikolaev@tradingview.com>
2024-04-09 14:03:44 +0300

    Add task_7.py and update task_6.py

--- Selenium/Selenium Python/6. Drag and drop/task_7.py
+++ Selenium/Selenium Python/6. Drag and drop/task_7.py
@@ -5,25 +5,25 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 chrome_options = Options()
-chrome_options.add_argument('--window-size=1920,1080')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-cache')
 chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 
-BALLS = ('xpath', '//div[contains(@class, "ball_color")]')
-RESULT = ('xpath', '//p[@class="message"]')
+BOXES = ('xpath', '//div[contains(@class, "ui-draggable")]')
+RESULT = ('xpath', '//p[@id="message"]')
 
 with webdriver.Chrome(options=chrome_options) as driver:
     wait = WebDriverWait(driver, 15, poll_frequency=1)
     actions = ActionChains(driver)
-    driver.get('https://parsinger.ru/selenium/5.10/4/index.html')
+    driver.get('https://parsinger.ru/selenium/5.10/8/index.html')
 
-    for ball in wait.until(EC.visibility_of_any_elements_located(BALLS)):
-        color = ball.get_attribute('class').split()[1].replace('_ball', '')
-        target = wait.until(
-            EC.visibility_of_element_located(('xpath', f'//div[contains(@class, "basket_color {color}")]'))
+    for box in wait.until(EC.visibility_of_any_elements_located(BOXES)):
+        ids = box.get_attribute('id').split('_')[-1]
+        colon = wait.until(
+            EC.visibility_of_element_located(('xpath', f'//div[@id="range_{ids}"]/p'))
         )
-        driver.execute_script("arguments[0].scrollIntoView();", ball)
-        actions.drag_and_drop(ball, target).perform()
+        driver.execute_script("arguments[0].scrollIntoView();", box)
+        actions.click_and_hold(box).move_to_element(colon).release().perform()
 
     result = wait.until(EC.visibility_of_element_located(RESULT)).text
     print(f'Result: {result}')
